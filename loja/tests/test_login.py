68a3e9ed024f18b1def69e0376cce9f53306cfd2
Paulo Felix <Paulo.felixper@gmail.com>
2025-11-15 15:23:22 -0300

    Update test_login.py

--- loja/tests/test_login.py
+++ loja/tests/test_login.py
@@ -2,8 +2,8 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 
 class TesteLoginAdmin(TestCase):
+    
     def setUp(self):
-        # cria o usuário correto
         User.objects.create_superuser(
             username='teste',
             email='teste@example.com',
@@ -11,13 +11,12 @@ class TesteLoginAdmin(TestCase):
         )
 
     def test_login_admin_deve_funcionar(self):
-        # aqui propositalmente usamos a senha ERRADA
+        
         response = self.client.post('/admin/login/?next=/admin/', {
             'username': 'teste',
-            'password': 'senha_errada'   # senha errada proposital
+            'password': 'senha_correta'   
         })
 
-        # o teste espera que o login funcione (MAS NÃO VAI FUNCIONAR)
-        # então o Jenkins VAI falhar — exatamente o que você quer
+        
         self.assertEqual(response.status_code, 302)
         self.assertIn('_auth_user_id', self.client.session)
