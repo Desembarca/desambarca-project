96843c63d35a3206b9fae6a62a352eeeda18327a
Paulo Felix <Paulo.felixper@gmail.com>
2025-11-15 02:47:13 -0300

    teste login

--- loja/tests/test_login.py
+++ loja/tests/test_login.py
@@ -1,32 +1,18 @@
 from django.test import TestCase
-from django.urls import reverse
 from django.contrib.auth.models import User
 
 class TesteLoginSenhaErrada(TestCase):
-
     def setUp(self):
-        # Usuário REAL com senha real
-        self.user = User.objects.create_user(
-            username="teste",
-            password="senha_correta"
+        User.objects.create_user(
+            username='teste',
+            password='senha_correta'
         )
 
     def test_login_com_senha_errada_deve_falhar(self):
-        # Faz login com SENHA ERRADA propositalmente
-        response = self.client.post(
-            reverse('login'),
-            {
-                'username': 'teste',
-                'password': 'senha_correta'
-            }
-        )
+        response = self.client.post('/login/', {
+            'username': 'teste',
+            'password': 'senha_errada'
+        })
 
-        # O login com senha errada NÃO autentica,
-        # então a sessão continua SEM o id de usuário logado.
-        # Mas aqui vamos esperar que o login dê certo de propósito.
-        
-        self.assertIn(
-            '_auth_user_id',
-            self.client.session,
-            "Falha proposital: senha errada não loga, este teste deve falhar."
-        )
+        session = self.client.session
+        self.assertNotIn('_auth_user_id', session)
